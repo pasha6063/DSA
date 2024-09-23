80434bbcc84d3b29b5b614afc25bdce5c67fc59d
Pasha <ghazanfarpasha125@gmail.com>
2024-09-23 23:01:31 +0500

    insertion

--- linked list/insertion.cpp
+++ linked list/insertion.cpp
@@ -35,6 +35,27 @@ void insertAtTail(Node *&head, int val)
         temp->next = new_node;
     }
 }
+void insertionAtPosition(Node* &head, int val,int pos)
+{
+    if(pos==0)
+    {
+        insertAtHead(head, val);
+        return;
+    }
+    Node *new_node = new Node(val);
+    Node* temp = head;
+    int current_pos=0;
+    while(current_pos!=pos-1)
+    {
+        temp = temp->next;
+        current_pos++;
+
+    }
+    new_node->next = temp->next;
+    temp->next = new_node;
+
+
+}
 void display(Node *head)
 {
     Node *temp = head;
@@ -53,6 +74,8 @@ int main()
     insertAtHead(head, 2);
     display(head);
     insertAtTail(head,3);
+    display(head);
+    insertionAtPosition(head, 4, 2);
     display(head);
     return 0;
 }
